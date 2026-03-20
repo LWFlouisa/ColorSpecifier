be6442b4c709a4203726a13413628c8f486e47dc
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-03-20 10:54:55 +0000

    Create inquire.rb

---
+++ inquire.rb
@@ -0,0 +1,8 @@
+def inquire
+  inquire(:qui,                                       "Qui est cette?",
+          :que,                                   "Quel est le temps?",
+          :ou,                                        "Ou es le cafe?",
+          :quand,                 "Quend vous et moi mange a le cafe?",
+          :pourqoui,          "Pourqoui avoir le ordinateur a minuit?",
+          :methode,  "Que methode avoir vous utilisent le ordinateur?")
+end
